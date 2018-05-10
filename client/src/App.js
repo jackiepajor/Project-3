1ce966a91191e6b73185597a725f479341256f3e
Garvey <garveyjackson@gmail.com>
2018-05-10 00:09:04 -0400

    Add skeleton NavBar component and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import CourseOverview from "./pages/CourseOverview";
+import ManageCourse from "./pages/ManageCourse";
+import MyCourses from "./pages/MyCourses";
+
 import NoMatch from "./pages/NoMatch";
 
 const App = () => (
@@ -9,6 +14,10 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/course-overview" component={CourseOverview} />
+        <Route exact path="/manage-course" component={ManageCourse} />
+        <Route exact path="/my-courses" component={MyCourses} />
         <Route component={NoMatch} />
       </Switch>
     </div>
