f6c5a116a3fec8d8ff5ca53fe5fdee4fe5b2565d
jesse <jessemspringer@gmail.com>
2018-05-12 12:16:06 -0400

    working on mongoDB email validation

--- server.js
+++ server.js
@@ -16,6 +16,23 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytreact");
 
+
+var userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    validate: {
+      validator: function(v, cb) {
+        User.find({email: v}, function(err,docs){
+           cb(docs.length == 0);
+        });
+      },
+      message: 'Email already exists!'
+    }
+  }
+});
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
---
+++ src/models/User.js
@@ -0,0 +1,44 @@
+var mongoose = require("mongoose");
+
+// reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// new UserSchema object
+var UserSchema = new Schema({
+
+  // `password` must be of type String
+  // `password` will trim leading and trailing whitespace before it's saved
+  // `password` is a required field and throws a custom error message if not supplied
+  // `password` uses a custom validation function to only accept values 6 characters or more
+//   password: {
+//     type: String,
+//     trim: true,
+//     required: "Password is Required",
+//     validate: [
+//       function(input) {
+//         return input.length >= 6;
+//       },
+//       "Password should be longer."
+//     ]
+//   },
+  // `email` must be of type String
+  // `email` must be unique
+  // `email` must match the regex pattern below and throws a custom error message if it does not
+  // You can read more about RegEx Patterns here https://www.regexbuddy.com/regex.html
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+  },
+  // `date` must be of type Date. The default value is the current date
+  userCreated: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var User = mongoose.model("User", UserSchema);
+
+// Export the User model
+module.exports = User;
