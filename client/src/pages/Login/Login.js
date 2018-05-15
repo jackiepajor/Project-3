98c646415e39a4cab7122a70832b83fb3f51d312
KurtisNiner <kcarpe28@gmail.com>
2018-05-14 20:03:03 -0400

    login stuff

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -11,18 +11,25 @@ class Login extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <h1>This is the Login Page</h1>
-=======
-        <NavBar />
-        <div class="jumbotron">
-          <h1>Welcome to Grasshopper</h1>
+        <div class="jumbotron center-align" id="jumbotron">
+          <h1>Grasshopper</h1>
           <h2>Let us teach you our ways</h2>
 
+          <form>
+            <label>
+              Name:
+    <input type="text" name="emailName" id="emailInput" />
+            </label>
+            <label>
+              Password:
+              <input type="text" name="passwordName" id="passwordInput" />
+            </label>
+          </form>
           <div class="button btn btn-primary" id="signIn">Sign In</div>
-          <div class="button btn btn-danger" id="logout">Logout</div>
+          <div class="button btn btn-success" id="createAccount">Create Account</div>
+
+
         </div>
->>>>>>> fa6a233c98306cab2e7d5489df6529c9d7868b1b
       </div>
     );
   }
