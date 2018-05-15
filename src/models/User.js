e25c2f834c513c00a295c14db619b7ce54125ba5
jesse <jessemspringer@gmail.com>
2018-05-14 21:09:45 -0400

    new models and fields

--- src/models/User.js
+++ src/models/User.js
@@ -6,6 +6,18 @@ var Schema = mongoose.Schema;
 // new UserSchema object
 var UserSchema = new Schema({
 
+  name: {
+    type: String
+  },
+
+  managedCourses: {
+    type: String
+  },
+
+  enrolledCourses: {
+    type: String
+  },
+
   email: {
     type: String,
     unique: true,
@@ -20,30 +32,21 @@ var UserSchema = new Schema({
     }
   },
 
-  // password: {
-  //   type: String,
-  //   trim: true,
-  //   required: "Password is Required",
-  //   validate: [
-  //     function(input) {
-  //       return input.length >= 6;
-  //     },
-  //     "Password should be longer."
-  //   ]
-  // },
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [
+      function(input) {
+        return input.length >= 6;
+      },
+      "Password should be longer."
+    ]
+  },
 
   
+});
 
-
-  
-
-  });
-//   // `date` must be of type Date. The default value is the current date
-//   userCreated: {
-//     type: Date,
-//     default: Date.now
-//   }
-// });
 
 // This creates our model from the above schema, using mongoose's model method
 var User = mongoose.model("User", UserSchema);
