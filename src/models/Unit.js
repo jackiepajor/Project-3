1c2b926ce187f04eba74d4b00037acea9b77bd49
jesse <jessemspringer@gmail.com>
2018-05-14 21:16:14 -0400

    added index.js file for exports

--- src/models/Unit.js
+++ src/models/Unit.js
@@ -26,7 +26,6 @@ var UnitSchema = new Schema ({
       }]
 
 
-
 })
 
 
--- src/models/index.js
+++ src/models/index.js
@@ -1,6 +1,11 @@
 
-// example to model later:
-// module.exports = {
-//     Book: require("./Book"),
-//     Library: require("./Library")
-//   };
+module.exports = {
+
+    User: require("./User"),
+    Course: require("./Course"),
+    Unit: require("./Unit"),
+    Lesson: require("./Lesson")
+
+  };
+
+  
