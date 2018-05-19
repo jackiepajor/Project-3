b6e21360e6d04265ada5b01dd496db848c8c1153
slunchice <slunchice_@hotmail.com>
2018-05-19 11:58:56 -0400

    Category file added

--- models/index.js
+++ models/index.js
@@ -4,7 +4,8 @@ module.exports = {
   User: require("./User"),
   Course: require("./Course"),
   Unit: require("./Unit"),
-  Lesson: require("./Lesson")
+  Lesson: require("./Lesson"),
+  Category: require("./Category")
 
 };
 
