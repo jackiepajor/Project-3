6d512e8d41117be8233c07e2a138272913e78ef4
Garvey <garveyjackson@gmail.com>
2018-05-19 13:11:16 -0400

    Add api methods to manipulate course resource paths

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,18 +1,49 @@
+import axios from 'axios';
+
 export default {
-  // Gets all books
-  getArticles: function() {
-    return fetch("/api/books");
+  getCourses: function() {
+    return axios.get("/api/course");
   },
-  // // Gets the book with the given id
-  // getBook: function(id) {
-  //   return axios.get("/api/books/" + id);
-  // },
-  // // Deletes the book with the given id
-  // deleteBook: function(id) {
-  //   return axios.delete("/api/books/" + id);
-  // },
-  // // Saves a book to the database
-  // saveBook: function(bookData) {
-  //   return axios.post("/api/books", bookData);
-  // }
+  getCourse: function(courseId) {
+    return axios.get(`/api/course/${courseId}`);
+  },
+  createCourse: function(data) {
+    return axios.post(`/api/course/${courseId}`, data);
+  },
+  deleteCourse: function(courseId) {
+    return axios.delete(`/api/course/${courseId}`);
+  },
+  updateCourse: function(courseId, data) {
+    return axios.put(`/api/course/${courseId}`, data);
+  },
+  getUnits: function(courseId) {
+    return axios.get(`/api/course/${courseId}/unit`);
+  },
+  getUnit: function(courseId, unitId) {
+    return axios.get(`/api/course/${courseId}/unit/${unitId}`);
+  },
+  createUnit: function(courseId, data) {
+    return axios.post(`/api/course/${courseId}/unit/${unitId}`, data);
+  },
+  deleteUnit: function(courseId, unitId) {
+    return axios.delete(`/api/course/${courseId}/unit/${unitId}`);
+  },
+  updateUnit: function(courseId, unitId, data) {
+    return axios.put(`/api/course/${courseId}/unit/${unitId}`, data);
+  },
+  getLessons: function(courseId, unitId) {
+    return axios.get(`/api/course/${courseId}/unit/${unitId}/lesson`);
+  },
+  getLesson: function(courseId, unitId, lessonId) {
+    return axios.get(`/api/course/${courseId}/unit/${unitId}/lesson/${lessonId}`);
+  },
+  createLesson: function(data) {
+    return axios.post(`/api/course/${courseId}/unit/${unitId}/lesson/`, data);
+  },
+  deleteLesson: function(courseId, unitId, lessonId) {
+    return axios.delete(`/api/course/${courseId}/unit/${unitId}/lesson/${lessonId}`, data);
+  },
+  updateLesson: function(courseId, unitId, lessonId, data) {
+    return axios.put(`/api/course/${courseId}/unit/${unitId}/lesson/${lessonId}`, data);
+  }
 };
