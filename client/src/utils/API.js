f1c7c32c367c9800c8117de0ff8c1efe627c873a
Ethan <erogers999@gmail.com>
2018-05-23 12:31:40 -0400

    update

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,56 +1,56 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export default {
+// export default {
 
-  // Course routes
-  getCourses: function() {
-    return axios.get("/api/course");
-  },
-  getCourse: function(courseId) {
-    return axios.get(`/api/course/${courseId}`);
-  },
-  createCourse: function(data) {
-    return axios.post(`/api/course/`, data);
-  },
-  deleteCourse: function(courseId) {
-    return axios.delete(`/api/course/${courseId}`);
-  },
-  updateCourse: function(courseId, data) {
-    return axios.put(`/api/course/${courseId}`, data);
-  },
+//   // Course routes
+//   getCourses: function() {
+//     return axios.get("/api/course");
+//   },
+//   getCourse: function(courseId) {
+//     return axios.get(`/api/course/${courseId}`);
+//   },
+//   createCourse: function(data) {
+//     return axios.post(`/api/course/`, data);
+//   },
+//   deleteCourse: function(courseId) {
+//     return axios.delete(`/api/course/${courseId}`);
+//   },
+//   updateCourse: function(courseId, data) {
+//     return axios.put(`/api/course/${courseId}`, data);
+//   },
 
-  // Unit routes
-  getUnits: function(courseId) {
-    return axios.get(`/api/course/${courseId}/unit`);
-  },
-  getUnit: function(courseId, unitId) {
-    return axios.get(`/api/course/${courseId}/unit/${unitId}`);
-  },
-  createUnit: function(courseId, data) {
-    return axios.post(`/api/course/${courseId}/unit/`, data);
-  },
-  deleteUnit: function(courseId, unitId) {
-    return axios.delete(`/api/course/${courseId}/unit/${unitId}`);
-  },
-  updateUnit: function(courseId, unitId, data) {
-    return axios.put(`/api/course/${courseId}/unit/${unitId}`, data);
-  },
+//   // Unit routes
+//   getUnits: function(courseId) {
+//     return axios.get(`/api/course/${courseId}/unit`);
+//   },
+//   getUnit: function(courseId, unitId) {
+//     return axios.get(`/api/course/${courseId}/unit/${unitId}`);
+//   },
+//   createUnit: function(courseId, data) {
+//     return axios.post(`/api/course/${courseId}/unit/`, data);
+//   },
+//   deleteUnit: function(courseId, unitId) {
+//     return axios.delete(`/api/course/${courseId}/unit/${unitId}`);
+//   },
+//   updateUnit: function(courseId, unitId, data) {
+//     return axios.put(`/api/course/${courseId}/unit/${unitId}`, data);
+//   },
 
-  // Lesson routes
-  getLessons: function(courseId, unitId) {
-    return axios.get(`/api/course/${courseId}/unit/${unitId}/lesson`);
-  },
-  getLesson: function(courseId, unitId, lessonId) {
-    return axios.get(`/api/course/${courseId}/unit/${unitId}/lesson/${lessonId}`);
-  },
-  createLesson: function(data) {
-    return axios.post(`/api/course/${courseId}/unit/${unitId}/lesson/`, data);
-  },
-  deleteLesson: function(courseId, unitId, lessonId) {
-    return axios.delete(`/api/course/${courseId}/unit/${unitId}/lesson/${lessonId}`, data);
-  },
-  updateLesson: function(courseId, unitId, lessonId, data) {
-    return axios.put(`/api/course/${courseId}/unit/${unitId}/lesson/${lessonId}`, data);
-  }
+//   // Lesson routes
+//   getLessons: function(courseId, unitId) {
+//     return axios.get(`/api/course/${courseId}/unit/${unitId}/lesson`);
+//   },
+//   getLesson: function(courseId, unitId, lessonId) {
+//     return axios.get(`/api/course/${courseId}/unit/${unitId}/lesson/${lessonId}`);
+//   },
+//   createLesson: function(data) {
+//     return axios.post(`/api/course/${courseId}/unit/${unitId}/lesson/`, data);
+//   },
+//   deleteLesson: function(courseId, unitId, lessonId) {
+//     return axios.delete(`/api/course/${courseId}/unit/${unitId}/lesson/${lessonId}`, data);
+//   },
+//   updateLesson: function(courseId, unitId, lessonId, data) {
+//     return axios.put(`/api/course/${courseId}/unit/${unitId}/lesson/${lessonId}`, data);
+//   }
   
-};
+// };
