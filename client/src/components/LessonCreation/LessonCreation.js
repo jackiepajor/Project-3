0f043c38a84c9091cbf143104d01ab736ec73f3f
Garvey <garveyjackson@gmail.com>
2018-05-30 17:22:13 -0400

    remove lesson creator how to placeholder text

--- client/src/components/LessonCreation/LessonCreation.js
+++ client/src/components/LessonCreation/LessonCreation.js
@@ -33,8 +33,6 @@ const LessonCreation = props => {
       <div className="container white-txt">
         <h3 className="display-3"><strong>Lesson Creator</strong></h3>
         <hr />
-        <blockquote><strong>How to </strong>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere temporibus mollitia eos ipsa dolore amet laboriosam distinctio. Non quidem tenetur a, rem aperiam exercitationem laborum ratione illum atque? Tempora mollitia ratione recusandae quo explicabo officiis dolores deleniti natus sunt aspernatur.</blockquote>
-        <hr />
       </div>
 
       <div className="jumbotron jumbotron-fluid">
@@ -54,27 +52,12 @@ const LessonCreation = props => {
         <div className="container">
           <div className="row">
             <div className="col-lg-12">
-              <div className="input-group mb-3">
-                  <div className="input-group-prepend">
-                      <button className="btn btn-hopper" type="button">Add</button>
-                  </div>
-                  <div className="custom-file" id="file-upload">
-                      <input type="file" className="custom-file-input"  />
-                      <label className="custom-file-label" for="inputGroupFile03">Choose file</label>
-                  </div>
-                    {/* <ReactQuill
-                      placeholder="Start Creating Your Lesson Here..." 
-                      value={props.lessonText}
-                      onChange={(value) => props.handleLessonChange(value)}
-                    /> */}
-                    </div>
-              </div>
               <div id="edit-container">
                     <Editor 
                     placeholder="Start Creating Your Lesson Here..." 
                     value={props.lessonText}
-                    onChange={(value) => props.handleLessonChange(value)}
-                    />
+                    onChange={(value) => props.handleLessonChange(value)}/>
+              </div>
             </div>
           </div>
         </div>
